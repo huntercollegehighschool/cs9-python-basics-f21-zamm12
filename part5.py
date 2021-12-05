4c4df1cd9df855121bd17a2dfcea5ff19f37bc99
Jonah Dunn <94643891+zamm12@users.noreply.github.com>
2021-12-05 21:54:27 +0000

    finshed part 5

--- part5.py
+++ part5.py
@@ -13,3 +13,6 @@ Hello, Ash Ketchum
 '''
 
 #start writing your code below
+firstname = input("Enter your first name: ")
+lastname = input("Enter  your last name: ")
+print("Hello", firstname, lastname )
